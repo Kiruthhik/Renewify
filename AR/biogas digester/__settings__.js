d6b5341d2b7fd4268c9f67f6e11f65f12e64583f
kiruthhik <kiruthhik@gmail.com>
2024-07-24 12:54:05 +0530

    AR

---
+++ AR/biogas digester/__settings__.js
@@ -0,0 +1,22 @@
+window.ASSET_PREFIX = "";
+window.SCRIPT_PREFIX = "";
+window.SCENE_PATH = "2033404.json";
+window.CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': false,
+    'preserveDrawingBuffer': false,
+    'deviceTypes': [`webgl2`, `webgl1`],
+    'powerPreference': "high-performance"
+};
+window.SCRIPTS = [ 188177360, 188177361, 188177384, 188177364, 188177365, 188177366, 188177367, 188177368, 188177370, 188177371, 188177372, 188177373, 188177375, 188177376 ];
+window.CONFIG_FILENAME = "config.json";
+window.INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+window.PRELOAD_MODULES = [
+    {'moduleName' : 'Ammo', 'glueUrl' : 'files/assets/188177381/1/ammo.wasm.js', 'wasmUrl' : 'files/assets/188177382/1/ammo.wasm.wasm', 'fallbackUrl' : 'files/assets/188177380/1/ammo.js', 'preload' : true},
+];
